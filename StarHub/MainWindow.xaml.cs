ad070e4cdda04b5bc78224d62420a2cd120c761e
James R Sconfitto <jugglingnutcase@gmail.com>
2013-11-02 13:14:15 -0400

    Initial commit
    Took me forever... doesn't even do anything. Here we go! :smile:

---
+++ StarHub/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using ReactiveUI;
+using ReactiveUI.Xaml;
+using StarHub.ViewModels;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StarHub
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window,  IViewFor<MainViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Initialize the VM
+            ViewModel = new MainViewModel();
+
+            // Bind things together
+            this.OneWayBind(ViewModel, vm => vm.UserName, view => view.UserName.Content);
+        }
+
+        public MainViewModel ViewModel
+        {
+            get { return (MainViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+        public static readonly DependencyProperty ViewModelProperty =
+           DependencyProperty.Register("ViewModel", typeof(MainViewModel), typeof(MainWindow));
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (MainViewModel)value; }
+        }
+    }
+}
